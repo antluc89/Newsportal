8ff2d1166b4d4bd80ff2ce547a50c2e85f34b09f
antluc89 <antluc89@yandex.by>
2025-01-22 07:22:00 -0500

    Module 17 - Models

---
+++ NewsPaper/news/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from django.contrib.auth.models import User
+from .resources import CATEGORIES, article
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        post_rate = self.post_set.aggregate(postRating=Sum('rating'))
+        prate = 0
+        prate += post_rate.get('postRating')
+
+        comment_rate = self.user.comment_set.aggregate(commentRating=Sum('rating'))
+        crate = 0
+        crate += comment_rate.get('commentRating')
+
+        self.rating = prate * 3 + crate
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    type = models.CharField(max_length=3, choices=CATEGORIES, default=article)
+    time = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[0:123] + '...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    time = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
